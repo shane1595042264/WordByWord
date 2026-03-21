43e34caf742b12fac5635e9af0ae16fa4fcea07e
Juntao Li <a1595042264@gmail.com>
2026-03-21 12:03:40 -0500

    KAN-37: fix PDF mode instantly marking sections as read in endofpage tracking mode

--- src/app/book/[id]/read/[sectionId]/page.tsx
+++ src/app/book/[id]/read/[sectionId]/page.tsx
@@ -449,7 +449,7 @@ export default function ReaderPage({ params }: { params: Promise<{ id: string; s
   const handleMarkedRead = useCallback(() => { refreshReadStatus() }, [refreshReadStatus])
 
   // Only track after loading completes to ensure scroll containers are mounted
-  useAutoTrack(sectionId, loading ? true : (section?.isRead ?? false), handleMarkedRead, contentRef, textScrollRef, viewMode)
+  useAutoTrack(sectionId, loading ? true : (section?.isRead ?? false), handleMarkedRead, contentRef, textScrollRef, viewMode, pdfScrollRef)
 
   const handlePageProgress = useCallback((currentPage: number, totalPages: number, scrollPercent: number) => {
     setSectionProgress(scrollPercent)
